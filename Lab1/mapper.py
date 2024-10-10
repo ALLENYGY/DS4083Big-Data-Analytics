e686c83c48d587060b3eb796eb381f6e85204a8c
AllenYGY <allenygy@outlook.com>
2024-10-10 20:13:32 +0800

    UPDATE

--- Lab1/mapper.py
+++ Lab1/mapper.py
@@ -1,10 +1,7 @@
 #!/opt/homebrew/Caskroom/miniforge/base/envs/BDA/bin/python3
 import sys
 
-# 读取标准输入的每一行
 for line in sys.stdin:
-    # 去除行首尾的空白并拆分成单词
     words = line.strip().split()
-    # 输出每个单词及计数 1
     for word in words:
         print(f"{word}\t1")
--- Lab1/reducer.py
+++ Lab1/reducer.py
@@ -5,12 +5,10 @@ import sys
 current_word = None
 current_count = 0
 
-# 从标准输入读取每一行
 for line in sys.stdin:
     word, count = line.strip().split('\t')
     count = int(count)
 
-    # 如果遇到新的单词，将前一个单词的计数输出
     if word == current_word:
         current_count += count
     else:
@@ -19,6 +17,11 @@ for line in sys.stdin:
         current_word = word
         current_count = count
 
-# 输出最后一个单词的计数
 if current_word is not None:
     print(f"{current_word}\t{current_count}")
+
+
+
+
+
+
